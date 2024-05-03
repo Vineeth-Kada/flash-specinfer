c498f41aa632d58d2ac1a74d1294a4c4d4a8a5a0
Vineeth-Kada <vineethkada@gmail.com>
2024-05-03 00:30:58 -0400

    Base Temp

--- python/bench.py
+++ python/bench.py
@@ -12,7 +12,9 @@ minimal_attn = load(
     name='minimal_attn',
     sources = list(map(lambda x: '../src/' + x, ['main.cpp', 'forward_1.cu', 'forward_2.cu', \
                                                  'forward_3.cu', 'forward_4.cu', 'forward_5.cu', \
-                                                 'forward_6.cu', 'forward_7.cu', 'forward_8.cu'])),
+                                                 'forward_6.cu', 'forward_7.cu', 'forward_8.cu',
+                                                 'forward_base_4.cu', 'forward_base_5.cu',
+                                                 'forward_base_6.cu', 'forward_base_8.cu'])),
     extra_cuda_cflags=['-O3', '--use_fast_math']
 )
 
@@ -24,7 +26,7 @@ num_tree_nodes = 2**10 - 10
 prompt_length = 10
 
 seq_len = num_tree_nodes + prompt_length
-IsTree = False
+IsTree = True
 
 q = torch.randn(batch_size, n_head, seq_len, head_embd, requires_grad=True).cuda()
 k = torch.randn(batch_size, n_head, seq_len, head_embd, requires_grad=True).cuda()
@@ -41,9 +43,10 @@ def torch_attention(q, k, v, mask):
     return output
 
 with torch.no_grad():
-    causal_mask = causal_masks.unsqueeze(1)#.broadcast_to((batch_size, n_head, seq_len, seq_len)).contiguous()
+    causal_mask = causal_masks.unsqueeze(1).broadcast_to((batch_size, n_head, seq_len, seq_len)).contiguous()
     if not IsTree:
         causal_mask = None
+    manual_result_torch = torch_attention(q, k, v, causal_mask)
     start, end = torch.cuda.Event(enable_timing=True), torch.cuda.Event(enable_timing=True)
     start.record()
     manual_result_torch = torch_attention(q, k, v, causal_mask)
@@ -51,10 +54,20 @@ with torch.no_grad():
     torch.cuda.synchronize()
 print("Time taken in ms: ", start.elapsed_time(end))
 
-print('\n\n=== profiling minimal flash attention (forward pass) === ')
+print('\n\n=== profiling minimal flash attention - ours (forward pass) === ')
 with torch.no_grad():
     for i in range(1, 8 + 1):
         forward_func = getattr(minimal_attn, f'forward_{i}')
         (minimal_result, time_taken, max_shared_mem, used_shared_mem) = forward_func(q, k, v, start_times, end_times, IsTree)
+        print(f"\033[1;34mIteration {i:3d}: \033[1;32mTime taken (ms): {time_taken.item():8.2f}; \033[1;35mSRAM Used: {used_shared_mem.item():6d}; \033[1;36mMax SRAM: {max_shared_mem.item():6d};\033[0m")
+        print('attention values sanity check:', torch.allclose(minimal_result, manual_result_torch, rtol=0, atol=1e-02))
+        
+if not IsTree:
+    causal_mask = torch.ones((batch_size, n_head, seq_len, seq_len), dtype=torch.bool).cuda().contiguous()
+print('\n\n=== profiling minimal flash attention - base (forward pass) === ')
+with torch.no_grad():
+    for i in [4, 5, 6, 8]:
+        forward_func = getattr(minimal_attn, f'forward_base_{i}')
+        (minimal_result, time_taken, max_shared_mem, used_shared_mem) = forward_func(q, k, v, causal_mask, IsTree)
         print(f"\033[1;34mIteration {i:3d}: \033[1;32mTime taken (ms): {time_taken.item():8.2f}; \033[1;35mSRAM Used: {used_shared_mem.item():6d}; \033[1;36mMax SRAM: {max_shared_mem.item():6d};\033[0m")
         print('attention values sanity check:', torch.allclose(minimal_result, manual_result_torch, rtol=0, atol=1e-02))
--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,19 @@ std::vector<torch::Tensor> forward_8(
     torch::Tensor Q, torch::Tensor K, torch::Tensor V, torch::Tensor StartTimes, torch::Tensor EndTimes, bool IsTree
 );
 
+std::vector<torch::Tensor> forward_base_4(
+    torch::Tensor Q, torch::Tensor K, torch::Tensor V, torch::Tensor CausalMask, bool IsTree
+);
+std::vector<torch::Tensor> forward_base_5(
+    torch::Tensor Q, torch::Tensor K, torch::Tensor V, torch::Tensor CausalMask, bool IsTree
+);
+std::vector<torch::Tensor> forward_base_6(
+    torch::Tensor Q, torch::Tensor K, torch::Tensor V, torch::Tensor CausalMask, bool IsTree
+);
+std::vector<torch::Tensor> forward_base_8(
+    torch::Tensor Q, torch::Tensor K, torch::Tensor V, torch::Tensor CausalMask, bool IsTree
+);
+
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     m.def("forward_1", &forward_1);
     m.def("forward_2", &forward_2);
@@ -34,4 +47,9 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     m.def("forward_6", &forward_6);
     m.def("forward_7", &forward_7);
     m.def("forward_8", &forward_8);
+    
+    m.def("forward_base_4", &forward_base_4);
+    m.def("forward_base_5", &forward_base_5);
+    m.def("forward_base_6", &forward_base_6);
+    m.def("forward_base_8", &forward_base_8);
 }
