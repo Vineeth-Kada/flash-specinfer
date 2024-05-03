dc9e50be839d1592772aaf21d6d67303f83c7f70
Vineeth-Kada <vineethkada@gmail.com>
2024-05-03 00:47:59 -0400

    Base Temp

--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,13 @@ std::vector<torch::Tensor> forward_8(
     torch::Tensor Q, torch::Tensor K, torch::Tensor V, torch::Tensor StartTimes, torch::Tensor EndTimes, bool IsTree
 );
 
+
+std::vector<torch::Tensor> forward_base_2(
+    torch::Tensor Q, torch::Tensor K, torch::Tensor V, torch::Tensor CausalMask, bool IsTree
+);
+std::vector<torch::Tensor> forward_base_3(
+    torch::Tensor Q, torch::Tensor K, torch::Tensor V, torch::Tensor CausalMask, bool IsTree
+);
 std::vector<torch::Tensor> forward_base_4(
     torch::Tensor Q, torch::Tensor K, torch::Tensor V, torch::Tensor CausalMask, bool IsTree
 );
@@ -32,6 +39,9 @@ std::vector<torch::Tensor> forward_base_5(
     torch::Tensor Q, torch::Tensor K, torch::Tensor V, torch::Tensor CausalMask, bool IsTree
 );
 std::vector<torch::Tensor> forward_base_6(
+    torch::Tensor Q, torch::Tensor K, torch::Tensor V, torch::Tensor CausalMask, bool IsTree
+);
+std::vector<torch::Tensor> forward_base_7(
     torch::Tensor Q, torch::Tensor K, torch::Tensor V, torch::Tensor CausalMask, bool IsTree
 );
 std::vector<torch::Tensor> forward_base_8(
@@ -48,8 +58,11 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     m.def("forward_7", &forward_7);
     m.def("forward_8", &forward_8);
     
+    m.def("forward_base_2", &forward_base_2);
+    m.def("forward_base_3", &forward_base_3);
     m.def("forward_base_4", &forward_base_4);
     m.def("forward_base_5", &forward_base_5);
     m.def("forward_base_6", &forward_base_6);
+    m.def("forward_base_7", &forward_base_7);
     m.def("forward_base_8", &forward_base_8);
 }
